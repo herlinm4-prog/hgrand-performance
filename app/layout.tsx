77458a5ede353d0c98cb43e598014691dc25f1e3
herlinm4-prog <herlinm4@gmail.com>
2026-09-24 02:54:45 -0400

    Add CMS layout

---
+++ app/layout.tsx
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'HGrand CMS',description:'Administración HGrand Performance'};
+export default function Layout({children}:{children:React.ReactNode}){return <html lang="es"><body>{children}</body></html>}
